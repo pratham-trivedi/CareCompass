201a286ba4487d32203cbce5eacb744b3da98010
Pratham Trivedi <146634604+pratham-trivedi@users.noreply.github.com>
2024-08-25 20:28:45 +0530

    Update apiRequest.js

--- CareCompass/client/src/lib/apiRequest.js
+++ CareCompass/client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: "https://carecompass-backend.onrender.com/api",
+    baseURL: import.meta.env.API_URL,
     withCredentials : true,
 });
 
