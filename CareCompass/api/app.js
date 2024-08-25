1fe3613060176e498ef9d8ac71fa98cad56255ef
Pratham Trivedi <146634604+pratham-trivedi@users.noreply.github.com>
2024-08-25 20:23:41 +0530

    Update app.js

--- CareCompass/api/app.js
+++ CareCompass/api/app.js
@@ -10,7 +10,14 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({origin: process.env.CLIENT_URL, credentials: true}))
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+ credentials: true
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
